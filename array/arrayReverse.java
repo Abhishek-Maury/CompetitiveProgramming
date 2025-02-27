9c66a28afefbf26f04df4572dfb6e69760e4c476
Abhishek <mauryaabhishek73115.com>
2025-02-27 16:40:06 +0530

    arrayReverse

---
+++ array/arrayReverse.java
@@ -0,0 +1,26 @@
+package array;
+
+// https://www.geeksforgeeks.org/program-to-reverse-an-array/
+public class arrayReverse {
+    public static void reverse(int[] arr){
+        int i=0;
+        int j=arr.length-1;
+        while (i<j) {
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        
+;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,4,3,2,6,5};
+       reverse(arr);
+       for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i]+" ");
+       }
+        
+    }
+}
