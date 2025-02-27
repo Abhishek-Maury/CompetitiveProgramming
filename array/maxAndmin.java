c36875909dc9088c3de9b9fa67acbf559b1a61c4
Abhishek <mauryaabhishek73115.com>
2025-02-27 15:03:35 +0530

    intial commit

---
+++ array/maxAndmin.java
@@ -0,0 +1,30 @@
+package array;
+// https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
+
+public class maxAndmin {
+    public static int max(int[] arr) {
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > ans) {
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+
+    public static int min(int[] arr){
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]<ans) {
+                ans=arr[i];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 4, 1, 9 };
+        System.out.println("maximum element is :" + max(arr));
+        System.out.println("minimum element is :" + min(arr));
+    }
+}
