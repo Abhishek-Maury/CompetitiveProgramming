a310ceb55f5fb3d0a0a0fd0e20547e0be91c60fb
Abhishek <mauryaabhi20051@gmail.com>
2025-05-20 10:03:25 +0530

    right view

--- BinaryTree/leftViewOfBinaryTree.java
+++ BinaryTree/leftViewOfBinaryTree.java
@@ -16,6 +16,27 @@ public class leftViewOfBinaryTree {
         }
 
     }
+
+      public ArrayList<Integer> leftView(Node root) {
+       ArrayList<Integer> result=new ArrayList<>();
+       rvbt(root, result, 0);
+       return result;
+
+    }
+
+    public void rvbt(Node curr,ArrayList<Integer> result,int level){
+        if (curr==null) {
+            return;
+        }
+        if (level==result.size()) {
+            result.add(curr.val);
+        }
+
+        
+        rvbt(curr.left, result, level+1);
+        rvbt(curr.right, result, level+1);
+    }
+
       public static void main(String[] args) {
         
     }
