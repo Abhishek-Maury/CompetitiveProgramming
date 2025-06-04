ab7edddca31b724aaabe6458a6f04f50396f5413
Abhishek <mauryaabhi20051@gmail.com>
2025-06-04 18:04:24 +0530

    Another day

---
+++ BST/InsertIntoABinarySearchTree.java
@@ -0,0 +1,47 @@
+package BST;
+
+import javax.swing.tree.TreeNode;
+
+public class InsertIntoABinarySearchTree {
+     class Node {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+            ;
+        }
+
+    }
+
+     public Node insertIntoBST(Node root, int data) {
+        if(root==null){
+            return new Node(data);
+        }
+        Node curr=root;
+        while(true){
+            if(curr.val<=data){
+                if(curr.right!=null){
+                    curr=curr.right;
+                }else{
+                    curr.right=new Node(data);
+                    break;
+                }
+            }else{
+                if(curr.left!=null){
+                    curr=curr.left;
+                }else{
+                    curr.left=new Node(data);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
