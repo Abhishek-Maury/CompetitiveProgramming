019fff703acd2e4a82368535ca6e2dc2f7939cd6
Abhishek <mauryaabhi20051@gmail.com>
2025-06-09 10:01:12 +0530

    modified

--- StackandQueue/NextGreaterElementll.java
+++ StackandQueue/NextGreaterElementll.java
@@ -3,7 +3,22 @@ package StackandQueue;
 import java.util.Stack;
 
 public class NextGreaterElementll {
-   
+    // leetcode 503
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> s=new Stack<>();
+        int n=nums.length;
+        int[] nge=new int[n];
+        for(int i=2*n-1;i>=0;i--){
+            while(!s.isEmpty() && s.peek()<=nums[i%n]){
+                s.pop();
+            }
+            if(i<n){
+                nge[i]=s.isEmpty()?-1:s.peek();
+            }
+            s.push(nums[i%n]);
+        }
+        return nge;
+    }
     public static void main(String[] args) {
         
     }
