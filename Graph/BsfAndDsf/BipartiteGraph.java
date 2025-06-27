af60a3561c22d281ea50cd52b62f8c7ddc7e9b06
Abhishek <mauryaabhi20051@gmail.com>
2025-06-27 09:44:14 +0530

    modifi

--- Graph/BsfAndDsf/BipartiteGraph.java
+++ Graph/BsfAndDsf/BipartiteGraph.java
@@ -33,11 +33,10 @@ public class BipartiteGraph {
 
         graph[3].add(new Edge(3, 1));
         graph[3].add(new Edge(3, 4));
-    
 
         graph[4].add(new Edge(4, 2));
         graph[4].add(new Edge(4, 3));
-  
+
     }
 
     public static boolean isBipartite(ArrayList<Edge>[] graph) {
@@ -69,10 +68,42 @@ public class BipartiteGraph {
         return true;
     }
 
+    // using DFS (Bipartite graph)
+    private static boolean dfs(ArrayList<Edge>[] graph, int curr, int col, int[] color) {
+        color[curr] = col;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (color[e.des] == -1) {
+                dfs(graph, e.des, 1 - col, color);
+            } else if (color[e.des] == col) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean dfsBipartite(ArrayList<Edge>[] graph) {
+        int[] color = new int[graph.length];
+
+        for (int i = 0; i < color.length; i++) {
+            color[i] = -1;
+        }
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] == -1) {
+                if (dfs(graph, i, 0, color) == false) {
+                    return false;
+                }
+                
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         int V = 5;
         ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
-        System.out.println(isBipartite(graph));
+        // System.out.println(isBipartite(graph));
+        System.out.println(dfsBipartite(graph));
     }
 }
